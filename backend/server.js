7b565bbed3c18ea1f78a2d41ad7f322866218d50
MichaelSoldatenko <mishasoldatenko2@gmail.com>
2025-02-12 20:02:12 +0200

    mongodb updates

--- backend/server.js
+++ backend/server.js
@@ -3,17 +3,23 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const authRoutes = require("./routes/authRoutes");
 const movieRoutes = require("./routes/movieRoutes");
+require("dotenv").config();
 
 const app = express();
 
 app.use(express.json());
 
-app.use(cors({ origin: "https://movie-app-xi-sage.vercel.app" }));
+app.use(cors({ origin: "http://localhost:3000" }));
 
-mongoose.connect("mongodb://localhost:27017/movieApp", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+const mongoURI = process.env.MONGO_URI || "mongodb://localhost:27017/movieApp";
+
+mongoose
+  .connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB Atlas"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 app.use("/api/auth", authRoutes);
 app.use("/api", movieRoutes);
