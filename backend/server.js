90c9e111cf08118821cf1145c84b2c8a2ef1939d
MichaelSoldatenko <mishasoldatenko2@gmail.com>
2025-02-13 15:49:22 +0200

    server.js updated

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors({ origin: "https://movie-app-psi-inky.vercel.app" }));
 
 const mongoURI = process.env.MONGO_URI || "mongodb://localhost:27017/movieApp";
 
